0245d4843f34a3ce174d6cdfff43f016e6a625d2
Samuel Williams <williamssamuel1999@gmail.com>
2022-08-16 14:26:56 +0100

    Bring in new flake, remove old system

--- format.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Extensions necessary to tell fourmolu about 
-EXTENSIONS="-o -XTypeApplications -o -XTemplateHaskell -o -XImportQualifiedPost -o -XPatternSynonyms -o -fplugin=RecordDotPreprocessor"
-SOURCES=$(git ls-tree -r HEAD --full-tree --name-only | grep -E '.*\.hs')
-fourmolu --mode inplace --check-idempotence $EXTENSIONS $SOURCES
-hlint $SOURCES
